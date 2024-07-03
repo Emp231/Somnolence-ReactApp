789be859ba9aa9b7e79b0af941364aecf91c5a3b
Samanyu Shrivastava <samshri06@gmail.com>
2024-07-02 18:56:09 -0700

    Added buttons to app

--- App.tsx
+++ App.tsx
@@ -1,28 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
 } from 'react-native';
 
 function App() {
-  return(
-    <Text>
-      Hello!
-    </Text>
-  )
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Get Started!</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Help</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>About</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  button: {
+    backgroundColor: '#3498db',
+    width: '80%', // Adjust the width as needed
+    marginBottom: 20,
+    paddingVertical: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+  },
+});
 
 export default App;
