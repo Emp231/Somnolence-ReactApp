3e9363b9f28ecab76cff2a14c26c7b5e313d4cea
Samanyu Shrivastava <samshri06@gmail.com>
2024-07-10 21:52:16 -0700

    implemented camera

--- GSscreen.tsx
+++ GSscreen.tsx
@@ -1,15 +1,20 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
+import { Camera, useCameraDevice, useCameraPermission } from "react-native-vision-camera";
 
 const GSscreen = () => {
+    
+    const device = useCameraDevice('back')
+    const { hasPermission, requestPermission } = useCameraPermission()
+
+    if(device == null) return <Text>NOCAMERA</Text>
+
     return(
-        <View>
-            <Text>
-                Hi!
-            </Text>
-        </View>
+        <Camera
+        device={device}
+        isActive={true} 
+        />
     )
 }
 
 export default GSscreen
-
